c4829c8fcb51e9297f3ffb1609fe4260aca9ceca
Fabio Utzig <fabio@utzig.net>
2013-04-11 07:49:12 -0300

    Change nested if-else to case for legibility

--- sevenseg/sevenseg.v
+++ sevenseg/sevenseg.v
@@ -16,35 +16,41 @@ reg   [3:0]   digit;
 
 // Anode
 always @(cnt[16:15]) begin
-	if      (cnt[16:15] == 2'b11) an1 <= 4'b1110;
-	else if (cnt[16:15] == 2'b10) an1 <= 4'b1101;
-	else if (cnt[16:15] == 2'b01) an1 <= 4'b1011;
-	else                          an1 <= 4'b0111;
+	case (cnt[16:15])
+		2'b11:   an1 <= 4'b1110;
+		2'b10:   an1 <= 4'b1101;
+		2'b01:   an1 <= 4'b1011;
+		default: an1 <= 4'b0111;
+	endcase
 end
 
 // Cathode
 always @(cnt[16:15] or digit0 or digit1 or digit2 or digit3) begin
-	if      (cnt[16:15] == 2'b00) digit <= digit0;
-	else if (cnt[16:15] == 2'b01) digit <= digit1;
-	else if (cnt[16:15] == 2'b10) digit <= digit2;
-	else                          digit <= digit3;
+	case (cnt[16:15])
+		2'b00:   digit <= digit0;
+		2'b01:   digit <= digit1;
+		2'b10:   digit <= digit2;
+		default: digit <= digit3;
+	endcase
 
-	if      (digit == 4'h0) seg1 <= 7'b1000000;
-	else if (digit == 4'h1) seg1 <= 7'b1111001;
-	else if (digit == 4'h2) seg1 <= 7'b0100100;
-	else if (digit == 4'h3) seg1 <= 7'b0110000;
-	else if (digit == 4'h4) seg1 <= 7'b0011001;
-	else if (digit == 4'h5) seg1 <= 7'b0010010;
-	else if (digit == 4'h6) seg1 <= 7'b0000010;
-	else if (digit == 4'h7) seg1 <= 7'b1111000;
-	else if (digit == 4'h8) seg1 <= 7'b0000000;
-	else if (digit == 4'h9) seg1 <= 7'b0010000;
-	else if (digit == 4'hA) seg1 <= 7'b0001000;
-	else if (digit == 4'hB) seg1 <= 7'b0000011;
-	else if (digit == 4'hC) seg1 <= 7'b1000110;
-	else if (digit == 4'hD) seg1 <= 7'b0100001;
-	else if (digit == 4'hE) seg1 <= 7'b0000110;
-	else                    seg1 <= 7'b0001110;
+	case (digit)
+		4'h0:    seg1 <= 7'b1000000;
+		4'h1:    seg1 <= 7'b1111001;
+		4'h2:    seg1 <= 7'b0100100;
+		4'h3:    seg1 <= 7'b0110000;
+		4'h4:    seg1 <= 7'b0011001;
+		4'h5:    seg1 <= 7'b0010010;
+		4'h6:    seg1 <= 7'b0000010;
+		4'h7:    seg1 <= 7'b1111000;
+		4'h8:    seg1 <= 7'b0000000;
+		4'h9:    seg1 <= 7'b0010000;
+		4'hA:    seg1 <= 7'b0001000;
+		4'hB:    seg1 <= 7'b0000011;
+		4'hC:    seg1 <= 7'b1000110;
+		4'hD:    seg1 <= 7'b0100001;
+		4'hE:    seg1 <= 7'b0000110;
+		default: seg1 <= 7'b0001110;
+	endcase
 end
 
 assign dp1 = ~((decplace[0] ^ cnt[15]) & (decplace[1] ^ cnt[16]));
